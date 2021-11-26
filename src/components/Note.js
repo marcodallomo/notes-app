4fce5fcb13efcb1294975043f079e6957c56b887
Marco Dall'Omo <marcodallomo@marcos-mbp.home>
2021-11-26 12:53:48 +0000

    first commit

---
+++ src/components/Note.js
@@ -0,0 +1,16 @@
+import { AiOutlineDelete } from "react-icons/ai";
+
+const Note = ({ id, text, date, handleDeleteNote }) => {
+  return (
+    <div className="note">
+      <span>{text}</span>
+      <div className="note-footer">
+        <small>{date}</small>
+
+        <AiOutlineDelete onClick={() => handleDeleteNote(id)} className="delete-icon" size="1.3em" />
+      </div>
+    </div>
+  );
+};
+
+export default Note;
